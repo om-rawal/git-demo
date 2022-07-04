d1c86795357a290932f37ca7364b1fbbd4c68409
Om <om.rawal@somaiya.edu>
2022-07-04 17:11:06 +0530

    modified login by shyam

--- login.java
+++ login.java
@@ -2,4 +2,10 @@ public class login{
 	public static void main(String[] args) {
 		System.out.print("Login");
 	}
+	public void singleSignon(){
+
+	}
+	public void usingOTP(){
+		System.out.print("OTP based");
+	}
 }
